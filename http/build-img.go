c5d171348d38df19f5130e02a571f5557ec25007
rich guo <rich.kuo@aceqilin.com>
2019-10-23 12:44:23 +0800

    update: api - buildImg, 修正 path 參數

--- http/build-img.go
+++ http/build-img.go
@@ -1,12 +1,23 @@
 package http
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+)
 
 var buildImgHandler = withUser(func(w http.ResponseWriter, r *http.Request, d *data) (int, error) {
 
+	paths, ok := r.URL.Query()["path"]
+	if !ok {
+		err := errors.New("can not get path in query string")
+		return errToStatus(err), err
+	}
+
+	path := paths[0]
+
 	err := d.RunHook(func() error {
 		return nil
-	}, "build", r.URL.Path, "", d.user)
+	}, "build", path, "", d.user)
 
 	if err != nil {
 		return errToStatus(err), err
